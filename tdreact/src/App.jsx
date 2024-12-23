695e2b60470750bf6b74fce9e5edb3c1e2482ec9
Ayman <aymanelkarroussi@gmail.com>
2024-12-23 15:42:58 +0100

    TD01 : Ajouter un composant pour le contenu principal de la page

--- tdreact/src/App.jsx
+++ tdreact/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+function MainContent() {
+  return <p> Ici, nous afficherons des informations interessantes :)</p>;
+}
 function Header() {
   return (
     <header>
@@ -23,6 +26,7 @@ function App() {
   return (
     <>
       <Header />
+      <MainContent />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
